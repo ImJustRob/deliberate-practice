10495a1e64ae52f0f4949cdb8313c4aa62e66a97
ImJustRob <robertalothian@hotmail.com>
2024-07-28 14:07:18 -0400

    Create practice.rb

---
+++ practice.rb
@@ -0,0 +1,9 @@
+#Write a program that uses a variable to store a word, then prints that word with all capital letters.
+x = "good morning"
+puts x.upcase
+
+#Write a program that uses variables to store two numbers, then prints the numbers added together.
+
+y = 5
+w = 6
+puts y + w
